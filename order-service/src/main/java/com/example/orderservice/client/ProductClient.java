ed68f76cac3ec84f4ec8647eddd9d898591fce10
Priyanshi <Pjadeja8658@conestogac.on.ca>
2026-02-06 14:20:45 -0500

    Fix url issues

--- order-service/src/main/java/com/example/orderservice/client/ProductClient.java
+++ order-service/src/main/java/com/example/orderservice/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-service")
+@FeignClient(name="product-service", url = "${product.service.url}")
 public interface ProductClient {
     @GetMapping("/api/products/{id}")
     ProductResponse getProductById(@PathVariable Long id);
